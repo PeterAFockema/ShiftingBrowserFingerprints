a8dd7281dafa72f1465bfc0cd93bb9c942fe1245
PeterAFockema <anonymous@person.com>
2026-07-12 23:15:21 +0100

    Added a restructuring to the ShiftingBrowserFingerprints file structure.

--- src/shiftingbrowserfingerprints/firefox_scrapers.py
+++ src/shiftingbrowserfingerprints/firefox_scrapers.py
@@ -1,13 +1,10 @@
 #scrapers_objects.py
 
-import time
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options as OptionsChrome
 from selenium.webdriver.firefox.options import Options as OptionsFirefox
-from bs4 import BeautifulSoup as soup
 import os
 
-class Scrapers(object):
+class FirefoxScrapers(object):
     '''
     A class for web scrapers.
     '''
@@ -15,66 +12,9 @@ class Scrapers(object):
     def __init__(self):
         print("The Scrapers class initialised...")
 
-    def chrome_driver_implementation(self):
-        # Initialise the Chrome driver
-        driver = webdriver.Chrome()
-        # Navigate to the URL
-        driver.get('https://google.com')
-        # Print the title page
-        print(driver.title)
-        # Here we close the browser when done
-        driver.quit()
-    
-    def chrome_driver_extension_implementation(self):
-        # cwd = os.getcwd()
-        # print("cwd: ", cwd)
-        file_being_run = os.path.dirname(os.path.abspath(__file__))
-        # print("file being run: ", file_being_run)
-        extension_location = str(file_being_run) + "/__assets__/extensions/hello_world.crx"
-        # print("extension location: ", extension_location)
-        chrome_options = OptionsChrome()
-        chrome_options.add_extension(extension_location)
-
-        # Initialise the Chrome driver
-        driver = webdriver.Chrome(options=chrome_options)
-        return driver
-
-    def chrome_driver_canvas_extension_implementation(self):
-        file_being_run = os.path.dirname(os.path.abspath(__file__))
-        extension_location = str(file_being_run) + "/__assets__/extensions/canvas.crx"
-        chrome_options = OptionsChrome()
-        chrome_options.add_extension(extension_location)
-
-        # Initialise the Chrome driver
-        driver = webdriver.Chrome(options=chrome_options)
-        return driver
-
     def firefox_driver_implementation(self):
         #Initialise the Firefox driver
         driver = webdriver.Firefox()
-        return driver
-
-    def firefox_driver_canvas_extension_implementation(self):
-        #Initialise the Firefox driver
-        options = OptionsFirefox()
-        driver = webdriver.Firefox(options=options)
-    
-        file_being_run = os.path.dirname(os.path.abspath(__file__))
-        extension_location = str(file_being_run) + "/__assets__/extensions/canv_ff/web-ext-artifacts/canv_ff.xpi"
-
-        driver.install_addon(extension_location)
-
-        return driver
-    
-    def firefox_driver_font_extension_implementation(self):
-        #Initialise the Firefox driver
-        options = OptionsFirefox()
-        driver = webdriver.Firefox(options=options)
-    
-        file_being_run = os.path.dirname(os.path.abspath(__file__))
-        extension_location = str(file_being_run) + "/__assets__/extensions/font_ff/web-ext-artifacts/font_ff.xpi"
-
-        driver.install_addon(extension_location)
         return driver
         
     def firefox_driver_extension_string_implementation(self, extension_choice: str):
@@ -299,64 +239,6 @@ class Scrapers(object):
                     driver.install_addon(extension_location, temporary=True)
                     extension_location = str(os.path.dirname(os.path.abspath(__file__)))
         return driver
-    
-    def firefox_driver_webgl_extension_implementation(self):
-        #Initialise the Firefox driver
-        options = OptionsFirefox()
-        driver = webdriver.Firefox(options=options)
-    
-        file_being_run = os.path.dirname(os.path.abspath(__file__))
-        extension_location = str(file_being_run) + "/__assets__/extensions/firefox_webgl.xpi"
-
-        driver.install_addon(extension_location, temporary=True)
-        return driver
-
-    def chrome_driver_implementation_passed_url(self, passed_url):
-        # Initialise the Chrome driver
-        driver = webdriver.Chrome()
-        # Navigate to the URL
-        driver.get(passed_url)
-        # Print the title page
-        print(driver.title)
-        # Here we close the browser when done
-        driver.quit()
-
-    def chrome_driver_implementation_passed_url_mobile(self, passed_url):
-        # Initialise the Chrome driver
-        driver = webdriver.Chrome()
-        driver.set_window_size(375, 667) #, 375) #Typical screen size for a mobile
-        driver.set_window_position(200, 200) # Move the window to position x/y
-        # Navigate to the URL
-        driver.get(passed_url)
-        # Print the title page
-        print(driver.title)
-        time.sleep(5)
-        print(soup(driver.page_source))
-        # Here we close the browser when done
-        driver.quit()
-    
-    def chrome_driver_implementation_passed_url_tablet(self, passed_url):
-        # Initialise the Chrome driver
-        driver = webdriver.Chrome()
-        driver.set_window_size(1024, 768) #Typical screen size for a tablet
-        # Navigate to the URL
-        driver.get(passed_url)
-        # Print the title page
-        print(driver.title)
-        print(driver.page_source)
-        # Here we close the browser when done
-        driver.quit()
-    
-    def chrome_driver_implementation_passed_url_desktop(self, passed_url):
-        # Initialise the Chrome driver
-        driver = webdriver.Chrome()
-        driver.set_window_size(1080, 1920) #Typical screen size for a desktop
-        # Navigate to the URL
-        driver.get(passed_url)
-        # Print the title page
-        print(driver.title)
-        # Here we close the browser when done
-        driver.quit()
 
     def firefox_driver_implementation_passed_url(self, passed_url):
         #Initialise the Firefox driver
@@ -399,29 +281,7 @@ class Scrapers(object):
         # Print the title page
         print(driver.title)
         # Here we close the browser when done
-        driver.quit()
-
-    def chrome_options(self):
-        options = OptionsChrome()
-        options.headless = True #Enable headless mode
-        options.add_argument("--window-size=1920, 1920") #Defined window browser size
-        return options   
-
-    def chrome_driver_implementation_passed_url_and_options(self, passed_url, options):
-        # Initialise the Chrome driver
-        driver = webdriver.Chrome(options=options)
-        # Navigate to the URL
-        driver.get(passed_url)
-        # Print the title page
-        print(driver.title)
-        # Here we close the browser when done
-        driver.quit()
-
-    def firefox_options(self):
-        options = OptionsFirefox()
-        options.headless = True #Enable headless mode
-        options.add_argument("--window-size=1920, 1920") #Defined window browser size
-        return options 
+        driver.quit() 
 
     def firefox_driver_implementation_passed_url_and_options(self, passed_url, options):
         #Initialise the Firefox driver
