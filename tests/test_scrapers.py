84f34de67cc442a045ee81d124d7dd9b2bbac664
PeterAFockema <anonymous@person.com>
2026-08-14 20:16:05 +0100

    Updated and added unit tests.

--- tests/test_scrapers.py
+++ tests/test_scrapers.py
@@ -1,10 +1,10 @@
 import unittest
-from shiftingbrowserfingerprints.scrapers import return_Scrapers_object
+from shiftingbrowserfingerprints.scrapers import return_Firefox_scrapers
 
 class TestFunctions(unittest.TestCase):
 
     def test_return_Scrapers_object(self):
-        return_Scrapers_object(self)
+        return_Firefox_scrapers(self)
 
 if __name__ == '__main__':
     unittest.main()
